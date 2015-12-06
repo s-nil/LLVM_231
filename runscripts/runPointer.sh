f49ae39fca8b276576c3cbe48f3e400c4845d307
salamanderrex <zhouqingyu.rex@gmail.com>
2015-12-05 20:50:24 -0800

    add test folder

---
+++ runscripts/runPointer.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+pass="pointerAnalysisOptimization"
+
+bench_path=$1
+
+#awk F "/" means seperated by /, print last column
+bench=` echo $bench_path | awk -F "/" '{print $NF}'`
+
+cd $bench_path
+clang -O0 -emit-llvm -c $bench_path/$bench/$bench.cpp -o $bench_path/$bench/$bench.bc
+
+llvm-dis $bench_path/$bench.bc
+
+#use 231.so to generate .bc
+echo $OUTPUTLOGS/$bench.pointer.log
+touch $OUTPUTLOGS/$bench.pointer.log
+opt -load $LLVMLIB/CSE231.so -$pass < $bench.bc -analyze >> $OUTPUTLOGS/$bench.pointer.log
+cat $OUTPUTLOGS/$bench.pointer.log
+
+
+
+#back to your place
+cd -

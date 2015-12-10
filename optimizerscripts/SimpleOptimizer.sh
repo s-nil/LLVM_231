e0debdb08ea8763b8d63bd249d870f44ed72adc0
salamanderrex <zhouqingyu.rex@gmail.com>
2015-12-10 02:09:08 -0800

    add optimizer....forget to chagne gitignore...

---
+++ optimizerscripts/SimpleOptimizer.sh
@@ -0,0 +1,4 @@
+${CSE231ROOT}/runscripts/runPointer.sh ${CSE231ROOT}/P2test/optimizerSimple
+cd ${CSE231ROOT}/logs/optimizerSimple
+python ${CSE231ROOT}/optimizerscripts/optimizer/miner.py optimizerSimple
+cd -
